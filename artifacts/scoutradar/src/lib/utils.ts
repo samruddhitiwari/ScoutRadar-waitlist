ac1c00fc44d469161386fef3b55d1a923fb860de
Samruddhi Tiwari <samruddhitiwari003@gmail.com>
2026-03-14 20:47:45 +0530

    flatten repo structure for vercel deploy

--- artifacts/scoutradar/src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/**
- * Utility to smartly merge tailwind classes
- */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
